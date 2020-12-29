b700050c14bf6a62973bbd1bd717c6f440c0eec7
Alejandro Loarca <ale.j.loarca@gmail.com>
2020-12-29 16:37:56 +0000

    Make simple server+prompt

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as express from 'express';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,6 +10,20 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "pinentry-vscode" is now active!');
+
+	const app = express();
+	const port = 46727;
+
+	app.get('/', async (req, res) => {
+		const pass = await vscode.window.showInputBox({
+			password: true,
+		});
+		res.status(200).send(pass);
+	});
+
+	app.listen(port, () => {
+		console.log(`Example app listening at http://localhost:${port}`);
+	});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
